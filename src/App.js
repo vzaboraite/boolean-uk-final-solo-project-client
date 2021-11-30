9d2b45ac8fea097bdb9a527917467492b3975b4a
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-30 18:25:47 +0000

    Setup main project routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router";
+
 function App() {
-  return;
+  return (
+    // TODO: replace h1 elements with components and pages
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/signup" element={<h1>Sign Up</h1>} />
+      <Route path="/login" element={<h1>Log In</h1>} />
+      <Route path="/lobby" element={<h1>Lobby</h1>} />
+      <Route path="/my-games" element={<h1>My Games</h1>} />
+      <Route path="/game/:id" element={<h1>Game</h1>} />
+    </Routes>
+  );
 }
 
 export default App;

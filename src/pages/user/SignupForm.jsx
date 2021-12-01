8d89ef0ee29c586f7cf98dc1874e60bc373c238c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-01 11:24:26 +0000

    Handle inputs, set user state, setup request -> handleSubmit

--- src/pages/user/SignupForm.jsx
+++ src/pages/user/SignupForm.jsx
@@ -1,12 +1,70 @@
+import { useState } from "react";
+
 export default function SignupForm() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    console.log(event.target.name, event.target.value);
+
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setUser({ ...user, [inputName]: inputValue });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted");
+
+    const userToCreate = {
+      ...user,
+    };
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userToCreate),
+    };
+
+    fetch(`${process.env.REACT_APP_API_URL}/signup`)
+      .then((res) => {
+        if (res.status === 400) {
+          throw Error("Missing information!");
+        } else if (res.status !== 200) {
+          throw Error(res);
+        }
+        return res.json();
+      })
+      .then(console.log)
+      .catch((error) => error);
+  };
+
+  console.log({ user });
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" required />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          required
+          onChange={handleInputChange}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          onChange={handleInputChange}
+        />
         <button type="submit">Submit</button>
       </form>
     </main>

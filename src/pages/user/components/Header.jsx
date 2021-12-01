a2bdb90e96131e0be73de14fb70204321fb85866
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-01 10:46:00 +0000

    Create `Header` component for navigation

---
+++ src/pages/user/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

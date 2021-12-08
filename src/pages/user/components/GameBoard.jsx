0309d18664c6ddfea15a472a917c4a4b46d5472c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-08 16:07:00 +0000

    Create `handleMove` function

--- src/pages/user/components/GameBoard.jsx
+++ src/pages/user/components/GameBoard.jsx
@@ -1,38 +1,74 @@
+import { useState } from "react";
+
+const initialBoard = [
+  [null, "red", null, "red"],
+  [null, null, null, null],
+  [null, null, null, null],
+  ["black", null, "black", null],
+];
 export default function GameBoard() {
-  const initialBoard = [
-    [null, "red", null, "red"],
-    [null, null, null, null],
-    [null, null, null, null],
-    ["black", null, "black", null],
-  ];
+  const [board, setBoard] = useState(initialBoard);
+  const [selectedPiece, setSelectedPiece] = useState(null);
 
   function buildRow(row, index) {
     const rowSquares = row.map((square, i) => (
       <div
+        key={i}
         className={`board-square ${
           (index + i) % 2 === 0 ? "sq-color__white" : "sq-color__grey"
         }`}
+        onClick={() => {
+          !square && selectedPiece && handleMove(index, i);
+        }}
       >
-        {drawPiece(square)}
+        {square && drawPiece(square, index, i)}
       </div>
     ));
 
-    return <div className="row">{rowSquares}</div>;
+    return (
+      <div key={index} className="row">
+        {rowSquares}
+      </div>
+    );
   }
 
   function buildBoard() {
-    const boardRows = initialBoard.map((row, index) => buildRow(row, index));
+    const boardRows = board.map((row, index) => buildRow(row, index));
     return <div className="board">{boardRows}</div>;
   }
 
-  function drawPiece(square) {
+  function drawPiece(square, rowIndex, colIndex) {
     return (
       <div
         className={`piece ${
           square !== null && (square === "red" ? "piece__red" : "piece__black")
         }`}
+        onClick={() => {
+          setSelectedPiece({ rowIndex, colIndex, color: square });
+        }}
       ></div>
     );
+  }
+
+  function handleMove(toRowIndex, toColIndex) {
+    const { rowIndex: fromRowIndex, colIndex: fromColIndex } = selectedPiece;
+
+    const updatedBoard = board.map((row, rowIndex) => {
+      return row.map((square, colIndex) => {
+        if (fromRowIndex === rowIndex && fromColIndex === colIndex) {
+          return null;
+        }
+
+        if (toRowIndex === rowIndex && toColIndex === colIndex) {
+          return selectedPiece.color;
+        }
+
+        return square;
+      });
+    });
+
+    setBoard(updatedBoard);
+    setSelectedPiece(null);
   }
 
   return <>{buildBoard()}</>;

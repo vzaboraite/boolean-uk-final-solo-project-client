93084e2aa5914348b353e13f939adca74e055c15
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-09 18:13:39 +0000

    Validate moves going down-left, down-right directions

--- src/utils/game.js
+++ src/utils/game.js
@@ -37,6 +37,35 @@ function getValidMoves(board, currentPosition, selectedPiece) {
     }
   }
 
+  //   move down-left direction
+  if (moveDown) {
+    const rowDown = board[fromRowIndex + 1];
+    console.log({ rowDown });
+    if (!rowDown) {
+      return validMoves;
+    }
+
+    const leftMove = rowDown[fromColIndex - 1];
+
+    if (leftMove === null) {
+      validMoves.push({
+        toRowIndex: fromRowIndex + 1,
+        toColIndex: fromColIndex - 1,
+      });
+    }
+
+    //  move down-right direction
+    const rightMove = rowDown[fromColIndex + 1];
+    console.log({ leftMove, rightMove });
+
+    if (rightMove === null) {
+      validMoves.push({
+        toRowIndex: fromRowIndex + 1,
+        toColIndex: fromColIndex + 1,
+      });
+    }
+  }
+
   return validMoves;
 }
 

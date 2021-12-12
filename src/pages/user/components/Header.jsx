75e3e286859521af67c77c0af191045917edec0a
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-12 22:58:05 +0000

    Update header: add dropdown-menu, conditional rendering

--- src/pages/user/components/Header.jsx
+++ src/pages/user/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function Header({ authUser }) {
   return (
     <header>
       <nav>
@@ -8,12 +8,32 @@ export default function Header() {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/login">Log In</Link>
-          </li>
+          {!authUser && (
+            <div className="auth-menu">
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+            </div>
+          )}
+          {authUser && (
+            <div className="dropdown">
+              <button className="link">{authUser.username}</button>
+              <div className="dropdown-menu">
+                <li>
+                  <Link to="/lobby">Lobby</Link>
+                </li>
+                <li>
+                  <Link to="/my-games">My games</Link>
+                </li>
+                <li>
+                  <Link to="/">Log Out</Link>
+                </li>
+              </div>
+            </div>
+          )}
         </ul>
       </nav>
     </header>
